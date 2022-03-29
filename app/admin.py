5bd79bb91009631c84a59be40d86579d04d32983
Tossen7 <munthar6@gmail.com>
2022-03-29 15:35:36 +0200

    Initial commit

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Member)
+admin.site.register(Category)
---
+++ app/models.py
@@ -0,0 +1,76 @@
+import email
+from email.policy import default
+from multiprocessing.sharedctypes import Value
+from tkinter import CASCADE
+from django.db import models
+
+# Create your models here.
+
+CHOICES = (
+	("1", "Available"),
+	("2", "Not Available")
+)
+
+WEIGHT = (
+	("1", "Box"),
+	("2", "Pieces")
+)
+
+class Brand(models.Model):
+	name = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.name
+
+class Category(models.Model):
+	name = models.CharField(max_length=255)
+	status = models.CharField(max_length=10, choices=CHOICES)
+
+	def __str__(self):
+		return self.name
+
+	
+class Product(models.Model):
+	date = models.DateField(auto_now_add=True)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	name = models.CharField(max_length=255)
+	# image = models.ImageField(upload_to="media/product/images/")
+	quantity = models.IntegerField(default=0)
+	weight = models.CharField(max_length=10, choices = WEIGHT, default=1)
+
+	def __str__(self):
+		return self.name
+
+
+
+class Member(models.Model):
+	firstname = models.CharField(max_length=250)
+	lastname = models.CharField(max_length=250)
+	username = models.CharField(max_length=250)
+	profile_pic = models.ImageField(default = 'dark-souls.jpg', upload_to = 'media')
+	email = models.EmailField()
+	password = models.CharField(max_length=250, null=False)
+	comfirm_password = models.CharField(max_length=250, null=False)
+	address = models.CharField(max_length=500)
+
+	def __str__(self) -> str:
+		return self.firstname
+
+
+class Order(models.Model):
+	user = models.ForeignKey(Member, on_delete=models.CASCADE, default = 1)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE, default=1)
+	number = models.IntegerField()
+	date = models.DateField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+	order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+	product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+	quantity = models.IntegerField()
+	rate = models.FloatField(max_length=100)
+	total = models.FloatField(max_length=100)
+	status = models.IntegerField()
+
+
